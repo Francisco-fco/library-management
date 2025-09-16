a338c7f85f82add438cbdf37d557e2fb7fcbe3f0
LAPTOP-QUKAQ9D7\frann <franne_@hotmail.com>
2025-09-16 21:26:41 +0200

    Add calculating logic to helpers based on case instructions

--- LibraryManagement/Common/Helpers/FeeCalculator.cs
+++ LibraryManagement/Common/Helpers/FeeCalculator.cs
@@ -1,6 +1,19 @@
 ﻿namespace LibraryManagement.Common.Helpers
 {
-    public class FeeCalculator
+    public static class FeeCalculator
     {
+        public const decimal MaxOutstandingFeesBeforeBorrowBlock = 100.00m;
+        private const int LoanPeriodDays = 14;
+        public const decimal LateFeePerDayPerBook = 5.00m;
+
+        public static bool CanMemberBorrow(decimal outstandingFees)
+        {
+            return outstandingFees <= MaxOutstandingFeesBeforeBorrowBlock;
+        }
+
+        public static DateTime CalculateDueDate()
+        {
+            return DateTime.Now.AddDays(LoanPeriodDays);
+        }
     }
 }
--- LibraryManagement/Common/Helpers/LoanCalculator.cs
+++ LibraryManagement/Common/Helpers/LoanCalculator.cs
@@ -1,6 +1,25 @@
 ﻿namespace LibraryManagement.Common.Helpers
 {
-    public class LoanCalculator
+    public static class LoanCalculator
     {
+        private const int StandardMemberMaxBooks = 3;
+        private const int PremiumMemberMaxBooks = 5;
+
+        public static bool CanMemberBorrowMoreBooks(int currentBooksCount, bool isPremium)
+        {
+            int maxBooks = GetMaxBooksForMember(isPremium);
+            return currentBooksCount < maxBooks;
+        }
+
+        public static int GetMaxBooksForMember(bool isPremium)
+        {
+            return isPremium ? PremiumMemberMaxBooks : StandardMemberMaxBooks;
+        }
+
+        public static int GetRemainingBorrowCapacity(int currentBooksCount, bool isPremium)
+        {
+            int maxBooks = GetMaxBooksForMember(isPremium);
+            return Math.Max(0, maxBooks - currentBooksCount);
+        }
     }
 }
